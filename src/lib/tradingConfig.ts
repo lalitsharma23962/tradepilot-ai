22d5b9fbb19215b612eb6ba5c3257087c3dbc3f8
lalitsharma23962 <lalitsharma23962@gmail.com>
2026-08-07 18:17:32 +0530

    Set canonical strategy version to v30

--- src/lib/tradingConfig.ts
+++ src/lib/tradingConfig.ts
@@ -1,7 +1,7 @@
 export type TradingMode='PAPER';
 export const TRADING_CONFIG={
  mode:'PAPER' as TradingMode,
- strategyVersion:'v29',
+ strategyVersion:'v30',
  minScore:90,
  ultraScore:96,
  researchMinRiskReward:1.5,
