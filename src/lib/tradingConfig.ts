267e05bee0fb5a36bba1878628b71186cced3d58
lalitsharma23962 <lalitsharma23962@gmail.com>
2026-08-07 19:12:07 +0530

    chore: bump strategy version to v32

--- src/lib/tradingConfig.ts
+++ src/lib/tradingConfig.ts
@@ -1,7 +1,7 @@
 export type TradingMode='PAPER';
 export const TRADING_CONFIG={
  mode:'PAPER' as TradingMode,
- strategyVersion:'v31',
+ strategyVersion:'v32',
  minScore:90,
  ultraScore:96,
  researchMinRiskReward:1.5,
