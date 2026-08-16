3e8875a5a082b6cdb31dd61c849689d686e6e014
lalitsharma23962 <lalitsharma23962@gmail.com>
2026-08-16 12:07:46 +0530

    Fix strategy type exports for Vercel build

--- src/lib/strategy.ts
+++ src/lib/strategy.ts
@@ -1,1 +1,2 @@
-export * from './strategyV39';
+export { evaluateStrategy, evaluateProductionStrategy, evaluateResearchStrategy } from './strategyV39';
+export type { StrategyConfig, StrategySignal, StrategySignalV39 } from './strategyV39';
