88545ef78d1e54bf866613c88ca63a1d12e8bea4
lalitsharma23962 <lalitsharma23962@gmail.com>
2026-08-21 02:54:45 +0530

    fix: export canonical StrategySignal alias

--- src/lib/strategy.ts
+++ src/lib/strategy.ts
@@ -1,2 +1,2 @@
 export { evaluateStrategy, evaluateProductionStrategy, evaluateResearchStrategy } from './strategyV39';
-export type { StrategyConfig, StrategySignalV39 } from './strategyV39';
+export type { StrategyConfig, StrategySignal, StrategySignalV39 } from './strategyV39';
